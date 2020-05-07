9410e7a8512262d5e80d29ca1abab7a6c090e811
SizweSeize <mpontshanesizwe@gmail.com>
2020-05-07 15:02:31 +0200

    Updated Animal

--- animal.js
+++ animal.js
@@ -1,53 +1,44 @@
-class Animal {
-
-    constructor(name) {
+class Animal{
+    constructor(name){
         this.name = name
     }
-    eat() {
-        console.log(this.name + ' eats')
-    }
-
-}
-
-class Dog extends Animal {
-    constructor(name) {
-        super(name)
-       
-    }
-
-    sound() {
-        console.log('Dog barks')
-    }
-}
-var dog = new Dog('Rax', 'Dog')
-dog.eat()
-dog.sound()
-
-
-class Cat extends Animal {
-    constructor(name) {
-        super(name)
-        
-    }
-    sound() {
-        console.log('Cat meows')
+    eat(){
+        console.log(this.name + ' eats');
     }
 }
 
-var cat = new Cat('Stormy', 'Cat')
-cat.eat()
+class Dog extends Animal{
+    constructor(name){
+        super(name);
+    }
+    sound(){
+        console.log('Dog barks');
+    }
+}
+var dog = new Dog('Rax', 'Dog');
+dog.eat();
+dog.sound();
+
+class Cat extends Animal{
+    constructor(name){
+        super(name);
+    }
+    sound(){
+        console.log('Cat meows');
+    }
+}
+
+var cat = new Cat('Stormy', 'Cat');
+cat.eat();
 cat.sound();
-
 
 let adoptedPets = [];
 
-class Home {
-
-    adoptPet(pet) {
+class Home{
+    adoptPet(pet){
         adoptedPets.push(pet);
     }
-
-    makeAllSounds() {
+    makeAllSounds(){
         for (var i = 0; i < adoptedPets.length; i++) {
             adoptedPets[i].sound();
         }
@@ -57,15 +48,15 @@ class Home {
 var home = new Home();
 var dog1 = new Dog();
 var dog2 = new Dog();
-var cat = new Cat()
+var cat = new Cat();
 
 home.makeAllSounds();
-home.adoptPet(dog1)
-home.makeAllSounds()
+home.adoptPet(dog1);
+home.makeAllSounds();
 
 
-home.adoptPet(cat)
-home.makeAllSounds()
+home.adoptPet(cat);
+home.makeAllSounds();
 
 home.adoptPet(dog2);
 home.makeAllSounds();
