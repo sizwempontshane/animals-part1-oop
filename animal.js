75a62631022bb158ec59fc37327b790f3fb8dd77
sizwempontshane <61079347+sizwempontshane@users.noreply.github.com>
2021-01-12 13:11:25 +0200

    updated changes

---
+++ animal.js
@@ -0,0 +1,63 @@
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  eat() {
+    console.log(this.name + " eats");
+  }
+  sound() {
+    console.log(sounds);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  sound() {
+    console.log("Dog barks");
+  }
+}
+var dog = new Dog("Rax");
+dog.eat();
+dog.sound();
+
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  sound() {
+    console.log("Cat meows");
+  }
+}
+
+var cat = new Cat("Stormy");
+cat.eat();
+cat.sound();
+
+class Home {
+  constructor() {
+    this.adoptedPets = [];
+  }
+  adoptPet(pet) {
+    this.adoptedPets.push(pet);
+  }
+  makeAllSounds() {
+    for (var i = 0; i < this.adoptedPets.length; i++) {
+      this.adoptedPets[i].sound();
+    }
+  }
+}
+
+var home = new Home();
+var dog1 = new Dog();
+var dog2 = new Dog();
+var cat = new Cat();
+
+home.makeAllSounds();
+home.adoptPet(dog1);
+home.makeAllSounds();
+home.adoptPet(cat);
+home.makeAllSounds();
+home.adoptPet(dog2);
+home.makeAllSounds();
