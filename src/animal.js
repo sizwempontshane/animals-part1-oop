ce3c89105d1f38da2219f403dcbdfced7c72650c
nandiKhumalo30 <nandi.khumalo@umuzi.org>
2021-01-27 20:22:36 +0200

    added tests in the spec file

--- src/animal.js
+++ src/animal.js
@@ -1,71 +1,39 @@
 class Animal {
-
     constructor(name) {
         this.name = name
     }
     eat() {
-        console.log('Food')
+        return 'Food'
     }
-
 }
 
 class Dog extends Animal {
     constructor(name) {
-        super(name)
-       
+        super(name)  
     }
-
     sound() {
-        console.log('Bark')
+        return 'Bark'
     }
 }
-// var dog = new Dog('Rax', 'Dog')
-// dog.eat()
-// dog.sound()
-
 
 class Cat extends Animal {
     constructor(name) {
-        super(name)
-        
+        super(name)    
     }
     sound() {
-        console.log('Meows')
+        return 'Meow'
     }
 }
 
-var cat = new Cat('Stormy', 'Cat')
-cat.eat()
-cat.sound();
-
-
-let adoptedPets = [];
-
 class Home {
-
     adoptPet(pet) {
         adoptedPets.push(pet);
     }
-
     makeAllSounds() {
+        let adoptedPets = [];
         for (var i = 0; i < adoptedPets.length; i++) {
             adoptedPets[i].sound();
         }
     }
 }
-
-var home = new Home();
-var dog1 = new Dog();
-var dog2 = new Dog();
-var cat = new Cat()
-
-home.makeAllSounds();
-home.adoptPet(dog1)
-home.makeAllSounds()
-
-
-home.adoptPet(cat)
-home.makeAllSounds()
-
-home.adoptPet(dog2);
-home.makeAllSounds();
+module.exports = { Dog, Cat, Home }
